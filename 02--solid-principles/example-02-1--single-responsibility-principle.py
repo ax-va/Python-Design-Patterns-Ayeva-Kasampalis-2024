360c39c39f761ffb347b84900e3c8df4a8380bb7
ax-va <vasiliev.informatik@t-online.de>
2024-08-27 09:40:35 +0200

    update

---
+++ 02--solid-principles/example-02-1--single-responsibility-principle.py
@@ -0,0 +1,62 @@
+"""
+Single responsibility principle (SRP) =
+Each class should have one job or responsibility,
+and that job should be encapsulated within that class.
+"""
+
+
+# example without SRP
+class Report:
+    """
+    Anti-example:
+    The Report class has two responsibilities:
+    1. generating a report;
+    2. saving the report's content to a file.
+    """
+
+    def __init__(self, content):
+        self.content = content
+
+    def generate(self):
+        print(f"Report generated: {self.content}")
+
+    def save_to_file(self, filename):
+        with open(filename, 'w') as file:
+            file.write(self.content)
+
+
+# example with SRP:
+# Separate responsibilities in two different classes,
+# each with only one responsibility.
+class Report:
+    """
+    Is responsible for generating the report's content.
+    """
+
+    def __init__(self, content):
+        self.content = content
+
+    def generate(self):
+        print(f"Report generated: {self.content}")
+
+
+class ReportSaver:
+    """
+    Is responsible saving the report's content to a file.
+    """
+
+    def __init__(self, report: Report):
+        self.report: Report = report
+
+    def save_to_file(self, filename: str):
+        with open(filename, 'w') as file:
+            file.write(self.report.content)
+
+
+if __name__ == "__main__":
+    report_content = "This is the content."
+    report = Report(report_content)
+    report.generate()
+
+    report_saver = ReportSaver(report)
+    report_saver.save_to_file("report.txt")
---
+++ 02--solid-principles/terms.md
@@ -0,0 +1,7 @@
+**SOLID = set of five design principles proposed by Robert C. Martin**
+
+- Single responsibility principle
+- Open-closed principle
+- Liskov substitution principle
+- Interface segregation principle
+- Dependency inversion principle
