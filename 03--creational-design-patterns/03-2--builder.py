c15c360d10ca796954659d5848de12941efaa459
ax-va <vasiliev.informatik@t-online.de>
2024-10-14 06:47:29 +0200

    update

--- 03--creational-design-patterns/03-2--builder.py
+++ 03--creational-design-patterns/03-2--builder.py
@@ -83,12 +83,10 @@ class Waiter:
 
 	@property
 	def steps(self):
-		return [
-			self.builder.prepare_dough,
-			self.builder.add_sauce,
-			self.builder.add_topping,
-			self.builder.bake,
-		]
+		return [self.builder.prepare_dough,
+				self.builder.add_sauce,
+				self.builder.add_topping,
+				self.builder.bake,]
 
 	@property
 	def pizza(self):
