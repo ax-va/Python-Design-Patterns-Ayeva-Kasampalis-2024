b8c5ac50c4c3c86546918da25d61ff38c4007b0b
ax-va <vasiliev.informatik@t-online.de>
2025-02-20 23:28:59 +0100

    update

--- 06--architectural-design-patterns/06-2-2--microservices--lanarky/large_language_model_service.py
+++ 06--architectural-design-patterns/06-2-2--microservices--lanarky/large_language_model_service.py
@@ -1,12 +1,10 @@
-import os
-
 import uvicorn
 from dotenv import load_dotenv
 from lanarky import Lanarky
 from lanarky.adapters.openai.resources import ChatCompletionResource
 from lanarky.adapters.openai.routing import OpenAIAPIRouter
 
-# Store the secret OpenAPI key as the environment variable `OPEN_API_KEY`.
+# Store the secret OpenAPI key as the environment `OPEN_API_KEY` variable.
 # The key is given in `.env` which is added to `.gitignore`.
 # See `README.md` for more information.
 load_dotenv()
