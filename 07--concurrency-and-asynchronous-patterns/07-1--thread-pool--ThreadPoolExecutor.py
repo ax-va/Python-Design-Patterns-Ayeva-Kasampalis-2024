0d5e66f63a38e850e85328743012076fe40361ef
ax-va <vasiliev.informatik@t-online.de>
2025-03-06 18:49:02 +0100

    update

---
+++ 07--concurrency-and-asynchronous-patterns/07-1--thread-pool--ThreadPoolExecutor.py
@@ -0,0 +1,44 @@
+"""
+*Worker threads* are used to offload processing tasks from the main thread.
+In the *Thread Pool* pattern, when one worker thread finishes a task,
+it does not terminate but goes back to the pool,
+awaiting another task that it can be used again for.
+"""
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def task(i):
+	""" This function simulates a task. """
+	print(f"Executing task {i}")
+	time.sleep(1)
+	print(f"Task {i} completed")
+
+
+with ThreadPoolExecutor(max_workers=5) as executor:
+	# Submit 10 tasks to the thread pool
+	for j in range(10):
+		executor.submit(task, j)
+	# Once a worker thread completes a task,
+	# it picks up another from the queue.
+
+# Executing task 0
+# Executing task 1
+# Executing task 2
+# Executing task 3
+# Executing task 4
+# Task 0 completed
+# Executing task 5
+# Task 1 completed
+# Executing task 6
+# Task 3 completed
+# Executing task 7
+# Task 2 completed
+# Executing task 8
+# Task 4 completed
+# Executing task 9
+# Task 5 completed
+# Task 6 completed
+# Task 9 completed
+# Task 8 completed
+# Task 7 completed
---
+++ 07--concurrency-and-asynchronous-patterns/terms.md
@@ -0,0 +1,14 @@
+## Concurrency and Asynchronous Patterns
+
+### Thread Pool
+
+*Worker threads* are used to offload processing tasks from the main thread.
+In the *Thread Pool* pattern, when one worker thread finishes a task, 
+it does not terminate but goes back to the pool, 
+awaiting another task that it can be used again for.
+
+### Worker Model
+
+### Future and Promise
+
+### Observer (in Reactive Programming)
