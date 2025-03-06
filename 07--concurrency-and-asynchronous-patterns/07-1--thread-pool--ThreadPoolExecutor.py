d523753407b2465eb1e62c0b298a701b054c575e
ax-va <vasiliev.informatik@t-online.de>
2025-03-06 18:58:37 +0100

    update

--- 07--concurrency-and-asynchronous-patterns/07-1--thread-pool--ThreadPoolExecutor.py
+++ 07--concurrency-and-asynchronous-patterns/07-1--thread-pool--ThreadPoolExecutor.py
@@ -10,35 +10,37 @@ from concurrent.futures import ThreadPoolExecutor
 
 def task(i):
 	""" This function simulates a task. """
-	print(f"Executing task {i}")
+	print(f"Executing task {i} ...")
 	time.sleep(1)
-	print(f"Task {i} completed")
+	print(f"Task {i} completed.")
 
 
 with ThreadPoolExecutor(max_workers=5) as executor:
 	# Submit 10 tasks to the thread pool
 	for j in range(10):
 		executor.submit(task, j)
+	# The tasks are executed concurrently
+	# using the threads available in the thread pool.
 	# Once a worker thread completes a task,
 	# it picks up another from the queue.
 
-# Executing task 0
-# Executing task 1
-# Executing task 2
-# Executing task 3
-# Executing task 4
-# Task 0 completed
-# Executing task 5
-# Task 1 completed
-# Executing task 6
-# Task 3 completed
-# Executing task 7
-# Task 2 completed
-# Executing task 8
-# Task 4 completed
-# Executing task 9
-# Task 5 completed
-# Task 6 completed
-# Task 9 completed
-# Task 8 completed
-# Task 7 completed
+# Executing task 0 ...
+# Executing task 1 ...
+# Executing task 2 ...
+# Executing task 3 ...
+# Executing task 4 ...
+# Task 0 completed.Task 2 completed.
+# Executing task 5 ...
+# Task 3 completed.
+# Executing task 6 ...
+# Task 1 completed.
+# Executing task 7 ...
+# Task 4 completed.
+# Executing task 8 ...
+#
+# Executing task 9 ...
+# Task 5 completed.
+# Task 6 completed.
+# Task 7 completed.
+# Task 8 completed.
+# Task 9 completed.
