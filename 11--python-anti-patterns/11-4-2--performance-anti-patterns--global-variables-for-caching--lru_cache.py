b081efc2c6cf684f08f5e123b9475c92cbe7ffc1
ax-va <vasiliev.informatik@t-online.de>
2025-03-23 14:59:48 +0100

    update

---
+++ 11--python-anti-patterns/11-4-2--performance-anti-patterns--global-variables-for-caching--lru_cache.py
@@ -0,0 +1,47 @@
+"""
+Use specialized caching libraries, for example, `lru_cache`
+instead of global variables for caching.
+"""
+import time
+import random
+from functools import lru_cache
+
+
+def query_db(user_id):
+	""" Simulates querying a database. """
+	time.sleep(random.uniform(1.0, 2.0))
+
+	user_data = {
+		1: {"name": "Alice", "email": "alice@example.com"},
+		2: {"name": "Bob", "email": "bob@example.com"},
+		3: {"name": "Charlie", "email": "charlie@example.com"},
+	}
+
+	result = user_data.get(user_id, {"error": "User not found"})
+	return result
+
+
+# not recommended
+_cache = {}  # global variable for caching
+
+
+def get_data_v1(user_id):
+	if user_id in _cache:
+		result = _cache[user_id]
+	else:
+		result = query_db(user_id)
+		_cache[user_id] = result
+	return result
+
+
+# better
+@lru_cache(maxsize=100)
+def get_data_v2(user_id):
+	return query_db(user_id)
+
+
+if __name__ == "__main__":
+	print(get_data_v1(1))
+	# {'name': 'Alice', 'email': 'alice@example.com'}
+	print(get_data_v2(1))
+	# {'name': 'Alice', 'email': 'alice@example.com'}
---
+++ terms.md
@@ -0,0 +1,19 @@
+### Audit trails
+
+Keeping a record of all changes made to a database for compliance.
+
+### Collaborative editing
+
+Allowing multiple users to edit a document simultaneously.
+
+### Least Recently Used (LRU)
+
+The `lru_cache` decorator provides the *Least Recently Used (LRU)* cache, 
+automatically managing the size and lifetime of cache entries.
+Also, it is thread-safe in a multithreaded environment 
+when multiple threads access or modify the cache simultaneously,
+and is optimized for performance.
+
+### Undo/redo features
+
+Providing the ability to undo or redo actions in an application.
