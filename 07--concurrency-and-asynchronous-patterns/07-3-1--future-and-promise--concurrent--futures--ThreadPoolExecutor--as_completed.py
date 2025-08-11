144f29deab5ca72d4a768a3919f4a0c6844629a0
Alexander Vasiliev 🐍 💪 <85578981+ax-va@users.noreply.github.com>
2025-08-11 22:16:02 +0200

    update

--- 07--concurrency-and-asynchronous-patterns/07-3-1--future-and-promise--concurrent--futures--ThreadPoolExecutor--as_completed.py
+++ 07--concurrency-and-asynchronous-patterns/07-3-1--future-and-promise--concurrent--futures--ThreadPoolExecutor--as_completed.py
@@ -35,7 +35,7 @@ def division_of_ten(x):
 
 
 def main():
-    with ThreadPoolExecutor(max_workers=64) as executor:
+    with ThreadPoolExecutor(max_workers=2) as executor:
         futures = [
             executor.submit(division_of_ten, i)
             for i in (1, 2, 0, 5, 10)
